c48001ab9af5c34894937e7d829f83b5d042c10a
omipapal12 <omipapal12@gmail.com>
2021-08-10 19:36:24 +0530

    Added: Avoidable Word

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -1,28 +1,29 @@
 package com.bridgelabz;
 
 public class HashTable {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         HashTableMain<String, Integer> hashMain = new HashTableMain();
-        String message = "“Paranoids are not\n" +
-                "paranoid because they are paranoid but\n" +
-                "because they keep putting themselves\n" +
-                "deliberately into paranoid avoidable\n" +
-                "situations”";
+        String message = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
 
         //String to String[] conversion.
         String[] messageArray = message.toLowerCase().split(" ");
-
         //Iterate within the array
         for(String word: messageArray) {
             Integer value =  hashMain.get(word);
+
             if( value == null)
                 value = 1;
             else
                 value = value + 1;
             hashMain.add(word , value);
         }
-        //Display the linked list
+        //Display the hashtable
+        System.out.println(hashMain);
+        //Remove "avoidable" from the hashtable
+        hashMain.remove("avoidable");
+        //Display the hashtable
         System.out.println(hashMain);
     }
-
 }
